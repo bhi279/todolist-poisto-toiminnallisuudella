6ea7e67f8ff734d106b1a126455270ef54a5869a
devaajanne <devaajanne@gmail.com>
2024-09-26 16:23:00 +0300

    Initial commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+function TodoList() {
+  const [todo, setTodo] = useState({ desc: "", date: "" });
+  const [todos, setTodos] = useState([]);
+
+  const handleDescChange = (event) => {
+    setTodo({ ...todo, desc: event.target.value });
+  };
+
+  const handleDateChange = (event) => {
+    setTodo({ ...todo, date: event.target.value });
+  };
+
+  const addTodo = () => {
+    if (todo.desc == "") {
+      alert("A todo cannot be empty.");
+    } else if (todo.date == "") {
+      alert("A todo must have a date.");
+    } else {
+      setTodos([...todos, todo]);
+      setTodo({ ...todo, desc: "", date: "" });
+    }
+  };
+
+  const deleteTodo = (event) => {
+    const deletedIndex = parseInt(event.target.value);
+    const newTodos = todos.filter((todo, index) => index !== deletedIndex);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Simple todolist</h1>
+      <h2>Add a todo</h2>
+      <form>
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          value={todo.desc}
+          onChange={handleDescChange}
+          name="description"
+        ></input>
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          value={todo.date}
+          onChange={handleDateChange}
+          name="date"
+        ></input>
+        <button type="button" onClick={addTodo}>
+          Add
+        </button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.desc}</td>
+              <td>{todo.date}</td>
+              <td>
+                <button type="button" value={index} onClick={deleteTodo}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TodoList;
